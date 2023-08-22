aab9a5103a907d3ecc457852142e45db704da503
Sahan <sahandilshan.projects.gmail.com>
2023-08-22 21:19:10 +0530

    theme added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,41 +1,105 @@
-// src/components/Navbar.js
 import React from "react";
 import { Link } from "react-router-dom";
 import { isAuthenticated, removeToken } from "../utils/Auth";
+import ToggleButton from "@mui/material/ToggleButton";
+import Brightness4Icon from "@mui/icons-material/Brightness4";
+import Brightness7Icon from "@mui/icons-material/Brightness7";
+import AppBar from "@mui/material/AppBar";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
 
-function Navbar() {
+function Navbar({ darkMode, toggleDarkMode }) {
   const handleLogout = () => {
     removeToken();
     // Redirect to login page
   };
 
+  const appBarStyle = {
+    position: "fixed", // Fixed positioning at the top
+    backgroundColor: darkMode ? "#f5f5f5" : "#333", // Adjust colors based on dark mode
+    boxShadow: "none", // Remove box shadow
+    top: "20px", // Add top margin
+    width: "90%", // Set width to 90% of the screen
+    left: "5%", // Center horizontally
+    borderRadius: "15px", // Add rounded corners
+    opacity: "0.9", // Make it slightly transparent
+  };
+
+  const containerStyle = {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+    padding: "0 16px", // Add padding if needed
+  };
+  const toggleButtonStyle = {
+    color: darkMode ? "black" : "inherit", // Change color based on dark mode
+  };
+
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
+    <AppBar style={appBarStyle}>
+      <Container style={containerStyle}>
+        <Typography
+          variant="h6"
+          component={Link}
+          to="/"
+          color={darkMode ? "black" : "inherit"}
+          style={{ textDecoration: "none", fontWeight: "bold" }}
+        >
+          TheJobs
+        </Typography>
         {isAuthenticated() ? (
           <>
-            <li>
-              <Link to="/user">User Dashboard</Link>
-            </li>
-            <li>
-              <Link to="/admin">Admin Dashboard</Link>
-            </li>
-            <li>
-              <button onClick={handleLogout}>Logout</button>
-            </li>
+            <Link
+              to="/user"
+              style={{
+                color: darkMode ? "black" : "inherit",
+                marginRight: "16px",
+              }}
+            >
+              User Dashboard
+            </Link>
+            <Link
+              to="/admin"
+              style={{
+                color: darkMode ? "black" : "inherit",
+                marginRight: "16px",
+              }}
+            >
+              Admin Dashboard
+            </Link>
+            <button
+              onClick={handleLogout}
+              style={{
+                color: darkMode ? "black" : "inherit",
+                background: "none",
+                border: "none",
+                cursor: "pointer",
+              }}
+            >
+              Logout
+            </button>
           </>
         ) : (
-          <>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-          </>
+          <Link
+            to="/login"
+            style={{
+              color: darkMode ? "black" : "inherit",
+              marginRight: "16px",
+            }}
+          >
+            Login
+          </Link>
         )}
-      </ul>
-    </nav>
+        <ToggleButton
+          value="darkMode"
+          selected={darkMode}
+          onChange={toggleDarkMode}
+          style={toggleButtonStyle}
+        >
+          {darkMode ? <Brightness7Icon /> : <Brightness4Icon />}
+        </ToggleButton>
+      </Container>
+    </AppBar>
   );
 }
 
